634d956b77fa36a1a8757bb050f5440b9c1ed3a2
christophmarckx <christoph.marckx@cegeka.com>
2021-04-23 21:03:35 +0200

    Fixed CORS issues #2

--- src/main/java/com/switchfully/pettinder/pet/PetController.java
+++ src/main/java/com/switchfully/pettinder/pet/PetController.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://amazing-benz-901ab5.netlify.app/")
+@CrossOrigin(origins = "https://amazing-benz-901ab5.netlify.app/pets")
 @RequestMapping(value = "pets")
 public class PetController {
 
